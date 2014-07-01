1b1031dde6c9bb928defc41f35ef59a2a6b0ddef
Christian Bodart <chris@bodar.com>
2014-07-01 12:36:07 +0100

    First run at refactor

---
+++ src/jason.path.js
@@ -0,0 +1,123 @@
+function JasonPath(path) {
+    this.path = path || this.path;
+    this.queryMethod = this.buildQuery(this.path);
+    this.lastResult = [];
+}
+JasonPath.prototype.path = "";
+JasonPath.prototype.normalizedPath = "";
+JasonPath.prototype.lastMode = null;
+JasonPath.prototype.queryObjects = function jasonPath_queryObjects(object) {
+    this.lastMode = "VALUE";
+    this.trace(this.normalizedPath.replace(/^\$;/, ""), object, "$");
+    return this.lastResult;
+};
+JasonPath.prototype.queryPaths = function jasonPath_queryPaths(object) {
+    this.lastMode = "PATH";
+    this.trace(this.normalizedPath.replace(/^\$;/, ""), object, "$");
+    return this.lastResult;
+};
+JasonPath.prototype.query = function jasonPath_query(object, returnAsPaths) {
+    if (!returnAsPaths) {
+        return this.queryObjects(object);
+    } else {
+        return this.queryPaths(object);
+    }
+};
+JasonPath.prototype.buildQuery = function jasonPath_buildQuery(pathAsString) {
+    this.normalizedPath = this.normalize(pathAsString);
+
+};
+JasonPath.prototype.normalize = function jasonPath_normalize(pathAsString) {
+    var subx = [];
+    return pathAsString.replace(/[\['](\??\(.*?\))[\]']/g, function($0, $1) {
+        return "[#" + (subx.push($1) - 1) + "]";
+    })
+        .replace(/'?\.'?|\['?/g, ";")
+        .replace(/;;;|;;/g, ";..;")
+        .replace(/;$|'?\]|'$/g, "")
+        .replace(/#([0-9]+)/g, function($0, $1) {
+            return subx[$1];
+        });
+};
+JasonPath.prototype.trace = function jasonPath_trace(expr, val, path) {
+    if (expr) {
+        var x = expr.split(";"),
+            loc = x.shift(),
+            jasonPath = this;
+        x = x.join(";");
+        if (val && val.hasOwnProperty(loc))
+            this.trace(x, val[loc], path + ";" + loc);
+        else if (loc === "*") {
+            this.walk(loc, x, val, path, function(m, l, x, v, p) {
+                this.trace(m + ";" + x, v, p);
+            });
+        } else if (loc === "..") {
+            this.trace(x, val, path);
+            this.walk(loc, x, val, path, function(m, l, x, v, p) {
+                typeof v[m] === "object" && jasonPath.trace("..;" + x, v[m], p + ";" + m);
+            });
+        } else if (/,/.test(loc)) { // [name1,name2,...]
+            for (var s = loc.split(/'?,'?/), i = 0, n = s.length; i < n; i++)
+                this.trace(s[i] + ";" + x, val, path);
+        } else if (/^\(.*?\)$/.test(loc)) { // [(expr)]
+            this.trace(this.eval(loc, val, path.substr(path.lastIndexOf(";") + 1)) + ";" + x, val, path);
+        } else if (/^\?\(.*?\)$/.test(loc)) { // [?(expr)]
+            this.walk(loc, x, val, path, function(m, l, x, v, p) {
+                if (jasonPath.eval(l.replace(/^\?\((.*?)\)$/, "$1"), v[m], m)) jasonPath.trace(m + ";" + x, v, p);
+            });
+        } else if (/^(-?[0-9]*):(-?[0-9]*):?([0-9]*)$/.test(loc)) { // [start:end:step]  phyton slice syntax
+            this.slice(loc, x, val, path);
+        }
+    } else
+        this.store(path, val);
+};
+
+JasonPath.prototype.walk = function jasonPath_walk(loc, expr, val, path, f) {
+    if (val instanceof Array) {
+        for (var i = 0, n = val.length; i < n; i++)
+            if (i in val)
+                f(i, loc, expr, val, path);
+    } else if (typeof val === "object") {
+        for (var m in val)
+            if (val.hasOwnProperty(m))
+                f(m, loc, expr, val, path);
+    }
+};
+
+JasonPath.prototype.slice = function jasonPath_slice(loc, expr, val, path) {
+    if (val instanceof Array) {
+        var len = val.length,
+            start = 0,
+            end = len,
+            step = 1;
+        loc.replace(/^(-?[0-9]*):(-?[0-9]*):?(-?[0-9]*)$/g, function($0, $1, $2, $3) {
+            start = parseInt($1 || start);
+            end = parseInt($2 || end);
+            step = parseInt($3 || step);
+        });
+        start = (start < 0) ? Math.max(0, start + len) : Math.min(len, start);
+        end = (end < 0) ? Math.max(0, end + len) : Math.min(len, end);
+        for (var i = start; i < end; i += step)
+            this.trace(i + ";" + expr, val, path);
+    }
+};
+JasonPath.prototype.asPath = function(path) {
+    var x = path.split(";"),
+        p = "$";
+    for (var i = 1, n = x.length; i < n; i++)
+        p += /^[0-9*]+$/.test(x[i]) ? ("[" + x[i] + "]") : ("['" + x[i] + "']");
+    return p;
+};
+JasonPath.prototype.eval = function jasonPath_eval(x, _v, _vname) {
+    try {
+        return $ && _v && eval(x.replace(/@/g, "_v"));
+    } catch (e) {
+        throw new SyntaxError("jsonPath: " + e.message + ": " + x.replace(/@/g, "_v").replace(/\^/g, "_a"));
+    }
+};
+JasonPath.prototype.store = function(p, v) {
+    if (p) {
+        this.lastResult.push(this.lastMode == "PATH" ? this.asPath(p) : v);
+    }
+    return !!p;
+};
---
+++ test/spec/jason.spec.js
@@ -0,0 +1,14 @@
+describe("jsonPath original tests", function() {
+    it("JASON can reproduce example 1 results", function() {
+        var jasonPath = new JasonPath("$..author"),
+            result = jasonPath.query(testJsonObject),
+            expectedResult = JSON.stringify(testResult1);
+        expect(expectedResult).toEqual(JSON.stringify(result));
+    });
+    it("JASON can reproduce example 2 results", function() {
+        var jasonPath = new JasonPath("$..author"),
+            result = jasonPath.query(testJsonObject, true),
+            expectedResult = JSON.stringify(testResult2);
+        expect(expectedResult).toEqual(JSON.stringify(result));
+    });
+});
